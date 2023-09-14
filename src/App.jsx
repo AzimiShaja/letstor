95cda19bbbbcd9d38285fcbe29606f0b2daa28b2
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2023-09-14 11:08:32 +0300

    create profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Router, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import ForgotPass from "./pages/ForgotPass";
 import Home from "./pages/Home";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
+import PrivateRoute from "./components/PrivateRoute";
 import SignUp from "./pages/SignUp";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -16,11 +16,12 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="offers" element={<Offers />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="forgot-password" element={<ForgotPass />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
       <ToastContainer
         position="bottom-center"
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,52 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "test",
+    email: "test@gmail.com",
+  });
+
+  const logoutHandler = () => {
+    auth.signOut();
+    navigate("/");
+  };
+  const { name, email } = formData;
+  return (
+    <>
+      <div className="bg-slate-900 text-white h-full flex flex-col items-center py-10 px-10">
+        <h1 className="text-4xl">My Profile</h1>
+        <div className="mt-10 w-full md:w-7/12 lg:w-6/12 xl:w-5/12">
+          <form className="flex flex-col gap-2 text-black">
+            <label className="text-white" htmlFor="">
+              Name:
+            </label>
+            <input type="text" id="name" value={name} />
+            <label className="text-white" htmlFor="">
+              Email:
+            </label>
+            <input type="email" id="email" value={email} />
+            <div className="flex justify-between items-center my-4 max-md:flex-col gap-3">
+              <p className="text-white text-center">
+                Do you want to change your name?
+                <span className="text-red-500 cursor-pointer"> Edit</span>{" "}
+              </p>
+              <p
+                onClick={logoutHandler}
+                className="text-white hover:text-red-500 duration-150 cursor-pointer"
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
